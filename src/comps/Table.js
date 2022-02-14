6fc7fc5d6c4dd5da7a7b0947dd10006c9172ecbe
pauliushak <shakalispaulius@gmail.com>
2022-02-14 15:01:31 +0200

    second commit

--- src/comps/Table.js
+++ src/comps/Table.js
@@ -1,45 +1,116 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {useSelector} from "react-redux";
 import {useDispatch} from "react-redux";
-import {circleDone} from "../features/redux1";
+import {greenCircleDone} from "../features/redux1";
+import {blueCircleDone} from "../features/redux1";
+import {setGreenPosition} from "../features/redux1"
+import {setBluePosition} from "../features/redux1";
+import {buyGreenProperty} from "../features/redux1";
+import {buyBlueProperty} from "../features/redux1";
+import {minusGreenMoney} from "../features/redux1";
+import {minusBlueMoney} from "../features/redux1";
 
 const Table = () => {
     const dispatch = useDispatch()
     const info = useSelector((state) => state.info.value);
-    const [getNum, setNum] = useState(1)
-    const [getDice, setDice] = useState(0)
+    const [showBtn, setBtn] = useState(false)
+    const findGreenCell = info.table.find(x => x.number === info.greenPosition)
+    const findBlueCell = info.table.find(x => x.number === info.bluePosition)
 
-    function rolled() {
 
-        const random = Math.ceil(Math.random() * 6)
-        setDice(random)
-        setNum(getNum + random)
-
+    function goGreen() {
+        dispatch(setGreenPosition(info.table))
+        setBtn(!showBtn)
     }
 
-    if (getNum > 16) {
-        dispatch(circleDone())
-        setNum(getNum-16)
+    if (info.greenPosition > 16) {
+        dispatch(greenCircleDone())
     }
+
+    function goBlue() {
+        dispatch(setBluePosition())
+        setBtn(!showBtn)
+    }
+
+    if (info.bluePosition > 16) {
+        dispatch(blueCircleDone())
+    }
+
+    function buyGreen(index, x) {
+        dispatch(buyGreenProperty({index, x}))
+    }
+
+    function buyBlue(index, x) {
+        dispatch(buyBlueProperty({index, x}))
+    }
+
+    function getColor(x) {
+        if (x.colorBought === 0) return "gold"
+        if (x.colorBought === 1) return "greenyellow"
+        if (x.colorBought === 2) return "#6fb1e0"
+    }
+
+
+    useEffect(() => {
+        if (findGreenCell) {
+            if (findGreenCell.colorBought === 2) {
+                alert("oops its BLUE player property,You have to pay FINE!")
+                dispatch(minusGreenMoney(findGreenCell.costs))
+            }
+        }
+    }, [info.greenPosition])
+
+
+    useEffect(() => {
+        if (findBlueCell) {
+            if (findBlueCell.colorBought === 1) {
+                alert("oops its GREEN player property,You have to pay FINE!")
+                dispatch(minusBlueMoney(findGreenCell.costs))
+            }
+        }
+    }, [info.bluePosition])
+
+
     return (
         <div>
             <div className="gameBoard">
                 {info.table.map((x, i) =>
-                    <div style={{backgroundColor: x !== 0 ? "green" : "red"}}
-                         key={i} className="box">
-                        <h1>{x}</h1>
-                        <div>
-                            {x === getNum && <img
-                                src="https://freepikpsd.com/file/2019/10/board-game-pieces-png-5-Transparent-Images.png"
-                                alt=""/>}
+                    <div onDoubleClick={showBtn ? () => buyBlue(i, x.number) : () => buyGreen(i, x.number)}
+                         style={{backgroundColor: getColor(x)}}
+                         key={i} className={`box ${x === 0 ? "rebeccapurple" : ''}`}>
+                        <h3>{x.number}</h3>
+                        <h3>Price:{x.costs}</h3>
+                        <div className="flex spc-a">
+                            <div>
+                                {x.number === info.greenPosition && <img
+                                    src="https://cdn1.iconfinder.com/data/icons/sports-2-6/48/52-512.png"
+                                    alt=""/>}
+                            </div>
+                            <div>
+                                {x.number === info.bluePosition && <img
+                                    src="https://cdn1.iconfinder.com/data/icons/sports-3-6/48/106-512.png"
+                                    alt=""/>}
+                            </div>
                         </div>
                     </div>)}
-                <h1>Your money: {info.money}</h1>
+
+
             </div>
-            <div className="flex centera centerj">
-                <button onClick={rolled}>ROLL</button>
-                <h2>You rolled:{getDice}</h2>
+            <div className="flex spc-a">
+
+                <div className="flex column">
+                    <h1>Green money: {info.greenPlayerMoney}</h1>
+                    <h1>Blue money: {info.bluePlayerMoney}</h1>
+                </div>
+                <div className="flex column">
+                    {showBtn && <button onClick={goGreen}>ROLL GREEN</button>}
+                    {!showBtn && <button className="blue" onClick={goBlue}>ROLL BLUE</button>}
+                    <h2>Green last rolled:{info.greenDiceRolled1}</h2>
+                    <h2>Blue last rolled:{info.greenDiceRolled2}</h2>
+                </div>
+
             </div>
+
 
         </div>
 
--- src/features/redux1.js
+++ src/features/redux1.js
@@ -5,18 +5,109 @@ export const changes = createSlice({
         name: "info",
         initialState: {
             value: {
-                table:[1,2,3,4,5,16,0,0,0,6,15,0,0,0,7,14,0,0,0,8,13,12,11,10,9],
-                money: 0,
+                table: [
+                    {number: 1, costs: 100, colorBought: 0},
+                    {number: 2, costs: 50, colorBought: 0},
+                    {number: 3, costs: 10, colorBought: 0},
+                    {number: 4, costs: 20, colorBought: 0},
+                    {number: 5, costs: 300, colorBought: 0},
+                    {number: 16, costs: 50, colorBought: 0},
+                    0, 0, 0,
+                    {number: 6, costs: 20, colorBought: 0},
+                    {number: 15, costs: 30, colorBought: 0},
+                    0, 0, 0,
+                    {number: 7, costs: 150, colorBought: 0},
+                    {number: 14, costs: 200, colorBought: 0},
+                    0, 0, 0,
+                    {number: 8, costs: 30, colorBought: 0},
+                    {number: 13, costs: 250, colorBought: 0},
+                    {number: 12, costs: 50, colorBought: 0},
+                    {number: 11, costs: 140, colorBought: 0},
+                    {number: 10, costs: 10, colorBought: 0},
+                    {number: 9, costs: 20, colorBought: 0}],
+                greenPlayerMoney: 0,
+                bluePlayerMoney: 0,
+                greenDiceRolled1: 0,
+                greenDiceRolled2: 0,
+                greenPosition: 1,
+                bluePosition: 1,
+
             }
         },
         reducers: {
-            circleDone:(state, action) => {
-                state.value.money += 200
+            minusBlueMoney: (state, action) => {
+                state.value.bluePlayerMoney -= (action.payload)/2
+                state.value.greenPlayerMoney += (action.payload)/2
             },
+            minusGreenMoney: (state, action) => {
+                state.value.greenPlayerMoney -= (action.payload)/2
+                state.value.bluePlayerMoney += (action.payload)/2
+            },
+            greenCircleDone: (state, action) => {
+                state.value.greenPlayerMoney += 200;
+                state.value.greenPosition -= 16
+            },
+            blueCircleDone: (state, action) => {
+                state.value.bluePlayerMoney += 200
+                state.value.bluePosition -= 16
+            },
+            setGreenPosition: (state, {payload}) => {
+                const random = Math.ceil(Math.random() * 6)
+                state.value.greenDiceRolled1 = random
+                state.value.greenPosition += random
+            },
+            setBluePosition: (state, action) => {
+                const random = Math.ceil(Math.random() * 6)
+                state.value.greenDiceRolled2 = random
+                state.value.bluePosition += random
+            },
+            buyGreenProperty: (state, {payload}) => {
+                if (state.value.table[payload.index].colorBought !== 2) {
+                    if (state.value.table[payload.index].colorBought === 1) {
+                        alert("YOU ALREADY OWN THIS PROPERTY!")
+                        return
+                    }
+                    if (state.value.greenPosition === payload.x) {
+                        if (state.value.greenPlayerMoney >= state.value.table[payload.index].costs) {
+                            state.value.table[payload.index].colorBought = 1
+                            state.value.greenPlayerMoney -= state.value.table[payload.index].costs;
+                        }
+                    }
+                } else {
+                    alert("THIS PROPERTY BELONGS TO BLUE PLAYER!")
+                }
+            },
+            buyBlueProperty: (state, {payload}) => {
+                if (state.value.table[payload.index].colorBought !== 1) {
+                    if (state.value.table[payload.index].colorBought === 2) {
+                        alert("YOU ALREADY OWN THIS PROPERTY!")
+                        return
+                    }
+                    if (state.value.bluePosition === payload.x) {
+                        if (state.value.bluePlayerMoney >= state.value.table[payload.index].costs) {
+                            state.value.table[payload.index].colorBought = 2
+                            state.value.bluePlayerMoney -= state.value.table[payload.index].costs;
+                        }
+                    }
+                } else {
+                    alert("THIS PROPERTY BELONGS TO GREEN PLAYER")
+                }
+            },
+
+
         }
         ,
     })
 ;
 
-export const {circleDone,roll} = changes.actions;
+export const {
+    greenCircleDone,
+    blueCircleDone,
+    setGreenPosition,
+    setBluePosition,
+    buyGreenProperty,
+    buyBlueProperty,
+    minusGreenMoney,
+    minusBlueMoney
+} = changes.actions;
 export default changes.reducer
